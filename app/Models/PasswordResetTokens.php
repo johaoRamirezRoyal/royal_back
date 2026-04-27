cc436605cc9391eb1eca133f43f81640eddee18b
Jhoni-dev <djhoniersamir@gmail.com>
2026-04-27 14:34:03 -0500

    model reset

---
+++ app/Models/PasswordResetTokens.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordResetTokens extends Model
+{
+    protected $table = 'password_reset_tokens';
+
+    protected $fillable = [
+        'email',
+        'token',
+        'used',
+    ];
+
+    const CREATE_AT = 'create_at';
+    const UPDATE_AT = 'update_at';
+}
