459806c83622860e106b85d3f31b680d01c37fc0
Jhoni-dev <djhoniersamir@gmail.com>
2026-06-12 08:33:48 -0500

    solucion en la encryptacion de cookies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prependToGroup('api', JwtFromCookie::class);
+        $middleware->encryptCookies(except: ['token', 'admissions_token']);
         $middleware->alias([
             'auth' => Authenticate::class,
             'system' => ValidateSystem::class,
