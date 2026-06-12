57d761155f5c9be7a31e0ffa53a21261c908bd32
Jhoni-dev <djhoniersamir@gmail.com>
2026-06-12 14:36:06 -0500

    correcion de importaciones sen SendRequestEmailAdmission

--- app/Listeners/SendRequestEmailAdmission.php
+++ app/Listeners/SendRequestEmailAdmission.php
@@ -4,6 +4,8 @@ namespace App\Listeners;
 
 use App\Events\RequestEmailAdmission;
 use App\Mail\RequestEmail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
