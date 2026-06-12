6cb280dcebe4afe68504a226cad370a84711d16b
Johao Ramirez <hernando.ramirez@royalschool.edu.co>
2026-06-12 10:14:51 -0500

    autoload

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -255,7 +255,7 @@ class AuthController extends Controller
                 return response()->json(['active' => false], 401);
             }
 
-            $user->load('perfilRelacion', 'nivelRelacion');
+            //$user->load('perfilRelacion', 'nivelRelacion');
 
             return response()->json([
                 'active' => true,
