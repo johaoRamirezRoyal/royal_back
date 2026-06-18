8a0ba7b52c8894b9d050a45f9131cab5ef02d13c
Johao Ramirez <hernando.ramirez@royalschool.edu.co>
2026-06-18 09:54:16 -0500

    update anio academico endpoint.

--- routes/api/anioAcademico.php
+++ routes/api/anioAcademico.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\AnioAcademico\AnioAcademico;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AnioAcademico::class, 'obtenerAniosAcademicos']);
+Route::get('/todos', [AnioAcademico::class, 'obtenerAniosAcademicos']);
 Route::get('/ultimo', [AnioAcademico::class, 'obtenerUltimoAnioAcademico']);
