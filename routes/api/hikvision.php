eefdc8c3e1869170eebb56ded381dbf0e7279ca3
Jhoni-dev <djhoniersamir@gmail.com>
2026-06-22 14:10:16 -0500

    obtener imagen de empleados, hikvision

--- routes/api/hikvision.php
+++ routes/api/hikvision.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::post("/", [HikvisionController::class, 'registrarEmpleadosMasivoPerfil']);
 Route::get('/testHikvision', [HikvisionController::class, 'testHikvisionConexion']);
 Route::get('/getList', [HikvisionController::class, 'obtenerEmpleadosRegistrados']);
+Route::get('/image', [HikvisionController::class, 'obtenerImagenEmpleado']);
 Route::get("/userInfo/perfil", [HikvisionController::class, 'obtenerEmpleadosRegistradosPorPerfil']);
 Route::get("/userInfo", [HikvisionController::class, 'obtenerUnEmpleadoEspecifico']);
 Route::delete("/perfil", [HikvisionController::class, 'eliminarUsuariosRegistrados']);
